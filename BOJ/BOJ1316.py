0c1697e793fd55eecab14928fa2e34999f8726ba
jiyoony <65219386+jiyoony@users.noreply.github.com>
2021-01-20 23:25:08 +0900

    Add files via upload

--- BOJ/BOJ1316.py
+++ BOJ/BOJ1316.py
@@ -1,1 +1,13 @@
-#그룹 다어 체커
+#그룹단어체커
+result=int(input())
+for i in range(result):
+    word=str(input())
+    for j in range(len(word)-1):
+        if word[j] == word[j+1]:
+            pass
+        elif word[j] in word[j+2:]:
+            result-=1
+            break
+print(result)
+
+# flag
